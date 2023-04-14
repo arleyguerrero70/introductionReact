56739a6f573b1e20dd41a0722a73a496bcb93553
Arley <aguerrero0615@gmail.com>
2023-04-13 22:19:28 -0500

    Feat: Hemos creado la maqueta del proyecto

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { ToDoCounter } from "./TodoCounter";
+import { ToDoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+const todos = [
+  {
+    text: "Tarea por hacer",
+    completed: false
+  },
+  {
+    text: "Tarea por hacer 2",
+    completed: true
+  },
+  {
+    text: "Tarea por hacer 3",
+    completed: false
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ToDoCounter />
+      <ToDoSearch />
+
+      {<TodoList>
+        {todos.map(todo =>(
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}
+        </TodoList>}
+      {<CreateTodoButton />}
+    </React.Fragment>
   );
 }
 
